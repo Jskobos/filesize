daf7a0ef00adc191550cd3566aaad26e87634302
Jared Kobos <jaredkobos@gmail.com>
2017-04-04 16:58:00 +0800

    Complete functionality

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const port = process.env.PORT || 3000
 const app = express()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+app.set('view engine', 'pug')
+app.use(require("connect-assets")())
 
 app.get('/', (req,res) => {
-  res.send('Hello!')
+  res.render('index', {title: "File Size", message: "Select a file to determine its size."})
+})
+
+app.post('/', upload.single('input-file'), (req,res) => {
+  res.json({
+    filename: req.file.originalname,
+    filesize: req.file.size
+  })
 })
 
 app.listen(port, () => {
